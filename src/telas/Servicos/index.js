3e807e204356d59950de67866e743e976792eb97
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-09-16 20:32:23 -0300

    Tela de serviços

---
+++ src/telas/Servicos/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { SafeAreaView, StatusBar, Text } from 'react-native';
+
+const servicos = [
+    {
+        id: 1,
+        nome: "Banho",
+        preco: 79.9,
+        descricao: "NÃO DE BANHO NO SEU GATO! Mas se precisar nós damos!"
+    },
+    {
+        id: 2,
+        nome: "Vacina V4",
+        preco: 89.9,
+        descricao: "Uma dose da vacina V4. Seu gato precisa de duas."
+    },
+    {
+        id: 3,
+        nome: "Vacina Antirrábica",
+        preco: 99.9,
+        descricao: "Uma dose da vacina antirrábica. Seu gato precisa de uma por ano."
+    }
+];
+
+export default function Servicos() {
+    return <SafeAreaView>
+        <StatusBar />
+        <Text>Serviços!</Text>
+    </SafeAreaView>
+}
