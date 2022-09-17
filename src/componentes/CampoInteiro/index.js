d491694bc7fc45e416e055a5ba6e70c1c0f6a4d9
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-09-16 21:22:14 -0300

    Add carrinho

--- src/componentes/CampoInteiro/index.js
+++ src/componentes/CampoInteiro/index.js
@@ -2,13 +2,26 @@ import React from 'react';
 
 import { TextInput } from 'react-native';
 
-export default function CampoInteiro({ valor, acao }) {
+import estilosPadrao from './estilos';
+
+export default function CampoInteiro({ valor, estilos, acao }) {
+    const atualiza = (novoValor, acaoRetorno) => {
+        const verificaInteiro = novoValor.match(/^[0-9]*$/);
+
+        if (!verificaInteiro) return;
+
+        const removeZeroEsquerda = novoValor.replace(/^(0)(.*)/, '$2');
+
+        acaoRetorno(removeZeroEsquerda);
+    }
+
     const numeroEmTexto = String(valor);
 
     return <TextInput
+        style={[estilosPadrao.campo, estilos]}
         keyboardType='number-pad'
         selectTextOnFocus
-        onChangeText={(novoValor) => {acao(novoValor)}}
+        onChangeText={(novoValor) => {atualiza(novoValor, acao)}}
         value={numeroEmTexto}
     />
 }
--- src/telas/Servicos/Item/index.js
+++ src/telas/Servicos/Item/index.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
 
 import CampoInteiro from '../../../componentes/CampoInteiro';
+import Botao from '../../../componentes/Botao';
 
 import estilos from './estilos';
 
@@ -19,14 +20,14 @@ export default function Item({ nome, preco, descricao }) {
             <View>
                 <View style={estilos.valor}>
                     <Text style={estilos.descricao}>Quantidade:</Text>
-                    <CampoInteiro valor={quantidade} acao={setQuantidade} />
+                    <CampoInteiro estilos={estilos.quantidade} valor={quantidade} acao={setQuantidade} />
                 </View>
                 <View style={estilos.valor}>
                     <Text style={estilos.descricao}>Preço:</Text>
                     <Text style={estilos.preco}>0</Text>
                 </View>
             </View>
-            <Button title="Adicionar" />
+            <Botao valor="Adicionar" acao={() => {}} />
         </View>
         <View style={estilos.divisor} />
     </>
