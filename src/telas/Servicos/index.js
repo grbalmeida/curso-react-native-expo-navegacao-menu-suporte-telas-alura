61c9cc8ca7bc0a4f40bc8a6165ff6ad167a41de5
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-09-17 07:15:30 -0300

    Moeda, Total e expandir

--- src/telas/Servicos/index.js
+++ src/telas/Servicos/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { SafeAreaView, StatusBar, Text, FlatList } from 'react-native';
+import { SafeAreaView, StatusBar, FlatList } from 'react-native';
 
 import Item from './Item';
 
@@ -28,7 +28,6 @@ const servicos = [
 export default function Servicos() {
     return <SafeAreaView>
         <StatusBar />
-        <Text>Serviços!</Text>
         <FlatList
             data={servicos}
             removeClippedSubviews={false}
