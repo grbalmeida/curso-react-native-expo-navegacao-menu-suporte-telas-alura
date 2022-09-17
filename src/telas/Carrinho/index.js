22607dc5e65f00e0da6ca8fc5c580d8ee0b66609
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-09-17 07:43:58 -0300

    Tela do Carrinho

--- src/telas/Carrinho/index.js
+++ src/telas/Carrinho/index.js
@@ -10,23 +10,26 @@ const servicos = [
         id: 1,
         nome: "Banho",
         preco: 79.9,
-        descricao: "NÃO DE BANHO NO SEU GATO! Mas se precisar nós damos!"
+        descricao: "NÃO DE BANHO NO SEU GATO! Mas se precisar nós damos!",
+        quantidade: 1
     },
     {
         id: 2,
         nome: "Vacina V4",
         preco: 89.9,
-        descricao: "Uma dose da vacina V4. Seu gato precisa de duas."
+        descricao: "Uma dose da vacina V4. Seu gato precisa de duas.",
+        quantidade: 2
     },
     {
         id: 3,
         nome: "Vacina Antirrábica",
         preco: 99.9,
-        descricao: "Uma dose da vacina antirrábica. Seu gato precisa de uma por ano."
+        descricao: "Uma dose da vacina antirrábica. Seu gato precisa de uma por ano.",
+        quantidade: 1
     }
 ];
 
-export default function Servicos() {
+export default function Carrinho() {
     return <TelaPadrao>
         <FlatList
             data={servicos}
